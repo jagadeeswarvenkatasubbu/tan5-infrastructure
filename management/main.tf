30c4f225c82ad905f967f5be0668687d98e5ef6a
jagadeeswarvenkatasubbu <jagadeeswarvenkatasubbu@gmail.com>
2021-02-08 18:55:33 +1100

    Initial commit

---
+++ management/main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = "tan5-management"
+  region  = "australia-southeast1"
+  zone    = "australia-southeast1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
