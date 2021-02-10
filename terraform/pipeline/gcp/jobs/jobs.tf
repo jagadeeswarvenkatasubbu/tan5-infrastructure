e47550b6c8cd700de69e69105f177c4b27c18a3f
jagadeeswarvenkatasubbu <jagadeeswarvenkatasubbu@gmail.com>
2021-02-10 19:37:20 +1100

    update jobs build.yaml

--- terraform/pipeline/gcp/jobs/jobs.tf
+++ terraform/pipeline/gcp/jobs/jobs.tf
@@ -1,27 +1,7 @@
 module "twentyeightdc" {
   source = "./../../../module/gcp/cloud_build"
 
-  name            = "28dc"
-  description     = "Build job to deploy the 28dc application"
-  git_branch_name = "main"
-  git_repo_name   = "github_jagadeeswarvenkatasubbu_28dc"
-  fileName        = "cloudbuild.yaml"
-}
-
-module "fortyeightdc" {
-  source = "./../../../module/gcp/cloud_build"
-
-  name            = "fortyeightdc"
-  description     = "Build job to deploy the 28dc application"
-  git_branch_name = "main"
-  git_repo_name   = "github_jagadeeswarvenkatasubbu_28dc"
-  fileName        = "cloudbuild.yaml"
-}
-
-module "thirtyeightdc" {
-  source = "./../../../module/gcp/cloud_build"
-
-  name            = "thirtyeightdc"
+  name            = "twentyeightdc"
   description     = "Build job to deploy the 28dc application"
   git_branch_name = "main"
   git_repo_name   = "github_jagadeeswarvenkatasubbu_28dc"
